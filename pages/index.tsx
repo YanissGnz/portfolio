b31066acae8cd5f57d3dd5c345de9c8da89f03c7
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2024-06-11 19:54:08 +0100

    added experience

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
-import Image from "next/image";
 import { Montserrat } from "next/font/google";
 
 import Header from "@/components/Header";
 
 import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Education from "@/components/Education";
+import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import Skills from "@/components/Skills";
-import Education from "@/components/Education";
 import Work from "@/components/Work";
-import Contact from "@/components/Contact";
 
 const openSans = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
 		<main
-			className={`flex flex-col items-center justify-between  ${openSans.className}`}
-		>
+			className={`flex flex-col items-center justify-between  ${openSans.className}`}>
 			<Header />
 			<Intro />
 			<About />
 			<Skills />
 			<Education />
+			<Experience />
 			<Work />
 			<Contact />
 		</main>
